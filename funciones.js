c167547305aab363669cf5ee15cb13f1640ff215
Adrian Ruiz <addsdeveloper@gmail.com>
2025-08-20 14:56:06 -0500

    feat: :sparkles: Enhance funciones.js with string manipulation functions and anonymous arrow functions

--- funciones.js
+++ funciones.js
@@ -48,9 +48,71 @@ console.log(decodeSpell('☽☽☽⚡')); // 101 (1 + 1 + (100 - 1))
 console.log(decodeSpell('☽⚕⚡')); // 49 (-1 - 50 + 100)
 
 //Set -> Evitar elementos duplicados las ordena....
+let xelemento = 2;
 const numeros = new Set([1,5,2,3,2]); // -> 2
 numeros.size
 numeros.has(xelemento)//  -> true / false
 numeros.delete(xelemento)// -> true / false
 numeros.add(xelemento)// -> Set
 numeros.clear()
+//Pendiente ⚠️
+
+let textos = "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas \"Letraset\", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum."
+
+//substring()
+let nuevaCadena = textos.substring(50, 100)
+console.log(nuevaCadena);
+
+let otraCadena = textos.slice(50, 100)
+console.log(otraCadena);
+
+/**
+ * Funcion para contar cadenas de texto
+ * @param {string} cadena 
+ * @param {string} separador 
+ * @returns {Array} array de valores
+ */
+function dividirTexto(cadena, separador) {
+    return cadena.split(separador)
+}
+
+let numerosConComas = "1,2,3,4,5,6,7,8,9,10,11";
+
+let valores = dividirTexto(numerosConComas, ",")
+console.log(valores);
+let totalPalabras = dividirTexto(textos, " ")
+console.log(`Total de palabras: ${totalPalabras.length}`);
+
+let nick = "_@adrian"
+let validarNick = nick.startsWith("@")
+let respuesta = validarNick ? "SI" : "NO"
+
+console.log(`El nick del usuario es valido? ${respuesta}`);
+
+let contiene500Anios = textos.includes("500 años")
+console.log(`Si contiene?${contiene500Anios ? "SI":"NO"}`);
+
+//Funciones de expresion anonimas
+const calcularEdad = function(edad) {
+    return edad >= 18;   
+}
+
+console.log(calcularEdad(16));
+
+//Funciones de expresion nombradas
+const factorial = function fact(n) {//1
+    if(n <= 1) return 1;//true
+    return n * fact( n - 1 ) //5 * 4 * 3 * 2 * 1
+}
+
+console.log(factorial(5)); // !5
+
+//Funciones tipo flecha
+
+function xOperacion(a) { console.log(a); return a * a;}
+const otraOperacionArrow = (a) => { console.log(a); return a * a; }
+const lightOperacionArrow = a => a * a;
+const sumarArrow = (a , b) => a + b;
+console.log(sumarArrow(20, 30));
+
+
